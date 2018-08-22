b3f1e0fd0491443eb884b724919f10e739f401e9
michelle <nzoudmich@yahoo.fr>
2018-08-22 00:49:18 -0700

    ajax call

--- js/app.js
+++ js/app.js
@@ -14,4 +14,22 @@ function loadNames(e) {
     
       //build the URL
     let url = 'http://uinames.com/api/?';
+    //read the origin and append to the URL
+    
+    if(origin !== '') {
+        url += `region=${origin}&`;
+    }
+    console.log(url);
+    //read the genre and append to the url
+        if(genre !== '') {
+        url += `gender=${genre}&`;
+    }
+    console.log(url);
+    // read the amount and append to the url
+       if(amount !== '') {
+        url += `amount=${amount}&`;
+    }
+    console.log(url);
+     //ajax call 
+    xhr = new XMLHttpRequest();
 }
