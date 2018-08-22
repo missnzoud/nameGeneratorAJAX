593aef2738b89eb9785e98725c0f4e6026b43cf6
michelle <nzoudmich@yahoo.fr>
2018-08-22 00:43:50 -0700

    read the value from the form and create variable

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 document.querySelector('#generate-names').addEventListener('submit', loadNames);
+
+function loadNames(e) {
+    e.preventDefault();
+    
+    //read the value from the form and create the variables;
+    
+    const origin = document.getElementById('country').value;
+    
+    const genre = document.getElementById('genre').value;
+    
+    const amount = document.getElementById('quantity').value;
+}
